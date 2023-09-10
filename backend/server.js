80b27039575c7895e9b05f07817a52d03c03081c
Prasad Sudrik <sudrikprasad90@gmail.com>
2023-09-10 20:42:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,8 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://chat-app-tak-a-tive-sk0h.onrender.com/",
   },
 });
 
